294891819dbc87db2600081da81c8411c2e1a906
fara003 <109841439+fara003@users.noreply.github.com>
2023-01-20 10:43:30 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+//выводим окно с сообщением
+let ask = prompt('enter your name')
+//переменная со строкой 
+let com=".com"
+
+//удаляем последние 3 буквы меняем на ".com"
+console.log(ask = ask.substring(0, ask.length-3) +com );
